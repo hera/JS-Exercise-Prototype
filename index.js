47c9d195a537ae4d2d443b0fde06ef980189295c
Alexander Heraimenka <a.heraimenka@gmail.com>
2020-04-22 13:43:32 -0700

    Task 3

--- index.js
+++ index.js
@@ -101,9 +101,18 @@ let car = new Car("Ford", "Focus");
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+    Person.call(this, name, age);
+    
+    this.favoriteToy = favoriteToy;
 }
+
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function () {
+    return `Playing with ${this.favoriteToy}`;
+}
+
 
 /* 
   TASK 4
